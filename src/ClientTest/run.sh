f2e3bffdf526dde4ae6249a3dac3352d44d13b04
oleg-rabaev <oleg@pubsubsql.com>
2014-02-05 17:16:44 -0400

    client test add shared manifest

--- src/ClientTest/run.sh
+++ src/ClientTest/run.sh
@@ -1,6 +1,6 @@
 javac -d . -cp .:../../lib/* ClientTest.java
-jar cvf ../../bin/ClientTest.jar *.class
-jar ufm ../../bin/ClientTest.jar manifest
+jar cvf ../../bin/ClientTest.jar ClientTest.class
+jar ufm ../../bin/ClientTest.jar ../manifest
 jar ufe ../../bin/ClientTest.jar ClientTest ClientTest.class
 java -jar ../../bin/ClientTest.jar
 

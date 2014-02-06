a3743df7caa41a4d5220b5b5106210c24f189279
oleg-rabaev <oleg@pubsubsql.com>
2014-02-06 00:44:47 -0400

    PubSubSQLGUI add build script

---
+++ src/PubSubSQLGUI/build.sh
@@ -0,0 +1,6 @@
+javac -d . -cp .:../../lib/* *.java
+jar cvf ../../bin/pubsubsqlgui.jar *.class  images/*.png   
+jar ufe ../../bin/pubsubsqlgui.jar PubSubSQLGUI PubSubSQLGUI.class
+jar ufm ../../bin/pubsubsqlgui.jar ../manifest 
+rm *.class
+

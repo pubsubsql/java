36a8b4beb088b7aa2cd1df9098e26eae9459afc4
oleg-rabaev <oleg@pubsubsql.com>
2014-02-09 13:15:56 -0500

    build add javadoc

--- src/Client/build.bat
+++ src/Client/build.bat
@@ -3,3 +3,7 @@ javac -d . -cp .;../../lib/gson-2.2.4.jar *.java
 jar cfv ../../lib/pubsubsql.jar pubsubsql/*.class
 
 
+
+javadoc Client.java
+jar cfv ../../lib/pubsubsql-javadoc.jar *.html resource/* pubsubsql/*html *.css package-list
+

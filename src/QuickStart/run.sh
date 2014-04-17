e1871152abb9ba9dbbd39dba11485977ee3f5340
Mike (Mykhailo) Oksenenko <mmx.mmx@gmail.com>
2014-04-17 04:59:38 -0400

    rm legacy

--- src/QuickStart/run.sh
+++
@@ -1,6 +0,0 @@
-javac -d . -cp .:../../lib/* QuickStart.java
-jar cvf ../../bin/QuickStart.jar QuickStart.class
-jar ufm ../../bin/QuickStart.jar ../manifest
-jar ufe ../../bin/QuickStart.jar QuickStart QuickStart.class
-java -jar ../../bin/QuickStart.jar
-
